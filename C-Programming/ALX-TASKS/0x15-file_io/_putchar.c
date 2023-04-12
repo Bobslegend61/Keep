8980e0b8b86f2057eab04da4c2f581ce5b5171b6
Emmanuel Alabi <bobslegend795@gmail.com>
2023-04-12 13:52:51 +0100

    ff

---
+++ C-Programming/ALX-TASKS/0x15-file_io/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _putchar - Prints out a character to stdout
+ * @c: Character to print
+ *
+ * Return: ASCII interger value of the character
+ */
+int _putchar(char c)
+{
+	putchar(c);
+	return (c);
+}
--- C-Programming/ALX-TASKS/test.c
+++ C-Programming/ALX-TASKS/test.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-// #include <unistd.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main (void)
 {
-	printf("%i\n", (1 << 5));
+	printf("%d %d - %d\n", O_RDONLY, O_WRONLY, (O_WRONLY << 1));
+	return 0;
 }
